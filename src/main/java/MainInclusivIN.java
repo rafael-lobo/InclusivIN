595bfcfa74eff6fd3621ad4c686438159f132476
rafael-lobo <rafaellpalmeira@hotmail.com>
2021-10-14 22:09:24 -0300

    Application properties e main

--- src/main/java/MainInclusivIN.java
+++ src/main/java/MainInclusivIN.java
@@ -1,5 +1,12 @@
-public class MainInclusivIN {
-    public static void main(String[] args) {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
+public class MainInclusivIN extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainInclusivIN.class, args);
     }
 }
